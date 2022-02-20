c47de4a56f9236b084467921a86fce6a392366d1
Manuel Luevano <manuelluevano@hotmail.com>
2022-02-20 12:12:37 -0600

    add package.json v2

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/app.css">
+    <title>Punto de Venta</title>
+</head>
+
+<body>
+
+    <h2>PUNTO DE VENTA</h2>
+
+</body>
+
+</html>
