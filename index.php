371aaf7b32f106be0b0888f5f0e43a26ed0b90e2
Manuel Luevano <manuelluevano@hotmail.com>
2022-02-20 13:19:52 -0600

    add package.json / index.php

--- index.php
+++ index.php
@@ -10,13 +10,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/app.css">
+    <link rel="stylesheet" href="src/normalize.css">
     <title>Punto de Venta</title>
 </head>
 
 <body>
 
     <h2>PUNTO DE VENTA</h2>
+
+    <form action="">
+
+        <label for="">Codigo de barras</label>
+        <input type="text" maxlength="13">
+
+
+    </form>
 
 </body>
 
